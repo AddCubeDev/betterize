04642bf0e5d92f468250c24a35703c3e53092f7d
Adamozo <naworsk.adam@gmail.com>
2023-04-14 03:17:35 +0200

    basic storyblok cms and blog integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from "astro/config";
 import windicss from "astro-windicss";
 import { astroImageTools } from "astro-imagetools";
+import storyblok from '@storyblok/astro';
 
 // import compressor from "astro-compressor";
 import critters from "astro-critters";
@@ -18,5 +19,20 @@ export default defineConfig({
         critters(),
         vue(),
         //partytown()
+        storyblok({
+            accessToken: 'AM2jY0VgbjITiaic9Z5RTgtt',
+            components: {
+              hero: 'components/Hero',
+              marketing1: 'components/Marketing1',
+              landingPage: 'storyblok/LandingPage',
+              blogPage: 'storyblok/BlogPage',
+              blogPost: 'components/subcomponents/BlogPost',
+              blogPostPreview: 'components/BlogPostList',
+              advantageItem: 'components/subcomponents/AdvantageItem',
+            },
+            apiOptions: {
+              region: 'eu',
+            },
+          }),
     ],
 });
