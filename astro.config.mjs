cb80a6602bbd29b60e9665562b8e45e7521c023b
ElDynamo <theDynamo2001@gmail.com>
2023-04-17 12:25:53 +0200

    Added Prefetch + minor changes in nav and testim.

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,32 +11,33 @@ import storyblok from "@storyblok/astro";
 import vue from "@astrojs/vue";
 import compressor from "astro-compressor";
 
+import prefetch from "@astrojs/prefetch";
+
 // https://astro.build/config
 export default defineConfig({
-    output: "static",
-    integrations: [
-        astroImageTools,
-        windicss(),
-
-        //critters(),
-        vue(),
-        //partytown()
-        storyblok({
-            accessToken: "AM2jY0VgbjITiaic9Z5RTgtt",
-            components: {
-                hero: "components/Hero",
-                marketing1: "components/Marketing1",
-                landingPage: "storyblok/LandingPage",
-                blogPage: "storyblok/BlogPage",
-                blogPost: "components/subcomponents/BlogPost",
-                blogPostPreview: "components/BlogPostList",
-                advantageItem: "components/subcomponents/AdvantageItem",
-            },
-            apiOptions: {
-                region: "eu",
-            },
-        }),
-        // purgecss(),
-        compressor({ gzip: false, brotli: true }),
-    ],
+  output: "static",
+  integrations: [astroImageTools, windicss(),
+  //critters(),
+  vue(),
+  //partytown()
+  storyblok({
+    accessToken: "AM2jY0VgbjITiaic9Z5RTgtt",
+    components: {
+      hero: "components/Hero",
+      marketing1: "components/Marketing1",
+      landingPage: "storyblok/LandingPage",
+      blogPage: "storyblok/BlogPage",
+      blogPost: "components/subcomponents/BlogPost",
+      blogPostPreview: "components/BlogPostList",
+      advantageItem: "components/subcomponents/AdvantageItem"
+    },
+    apiOptions: {
+      region: "eu"
+    }
+  }),
+  // purgecss(),
+  compressor({
+    gzip: false,
+    brotli: true
+  }), prefetch()]
 });
