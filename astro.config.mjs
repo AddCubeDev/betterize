2e8fe79387c7f15c5bdb4e8d37aec327e5271dca
ElDynamo <theDynamo2001@gmail.com>
2023-04-17 21:05:21 +0200

    Removing @astro-prefetch

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import windicss from "astro-windicss";
 import { astroImageTools } from "astro-imagetools";
 import storyblok from "@storyblok/astro";
 // import critters from "astro-critters";
-
 // import compressor from "astro-compressor";
 // import partytown from "@astrojs/partytown";
 // import purgecss from "astro-purgecss";
@@ -11,33 +10,34 @@ import storyblok from "@storyblok/astro";
 import vue from "@astrojs/vue";
 import compressor from "astro-compressor";
 
-import prefetch from "@astrojs/prefetch";
-
 // https://astro.build/config
 export default defineConfig({
-  output: "static",
-  integrations: [astroImageTools, windicss(),
-  //critters(),
-  vue(),
-  //partytown()
-  storyblok({
-    accessToken: "AM2jY0VgbjITiaic9Z5RTgtt",
-    components: {
-      hero: "components/Hero",
-      marketing1: "components/Marketing1",
-      landingPage: "storyblok/LandingPage",
-      blogPage: "storyblok/BlogPage",
-      blogPost: "components/subcomponents/BlogPost",
-      blogPostPreview: "components/BlogPostList",
-      advantageItem: "components/subcomponents/AdvantageItem"
-    },
-    apiOptions: {
-      region: "eu"
-    }
-  }),
-  // purgecss(),
-  compressor({
-    gzip: false,
-    brotli: true
-  }), prefetch()]
+    output: "static",
+    integrations: [
+        astroImageTools,
+        windicss(),
+        //critters(),
+        vue(),
+        //partytown()
+        storyblok({
+            accessToken: "AM2jY0VgbjITiaic9Z5RTgtt",
+            components: {
+                hero: "components/Hero",
+                marketing1: "components/Marketing1",
+                landingPage: "storyblok/LandingPage",
+                blogPage: "storyblok/BlogPage",
+                blogPost: "components/subcomponents/BlogPost",
+                blogPostPreview: "components/BlogPostList",
+                advantageItem: "components/subcomponents/AdvantageItem",
+            },
+            apiOptions: {
+                region: "eu",
+            },
+        }),
+        // purgecss(),
+        compressor({
+            gzip: false,
+            brotli: true,
+        }),
+    ],
 });
