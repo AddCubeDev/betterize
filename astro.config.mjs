a0f001c7c4c54bda4dbaa606d9f22a3172d058f9
Adamozo <naworsk.adam@gmail.com>
2023-04-19 00:56:31 +0200

    remove storyblok

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from "astro/config";
 import windicss from "astro-windicss";
 import { astroImageTools } from "astro-imagetools";
-import storyblok from "@storyblok/astro";
 // import critters from "astro-critters";
 // import compressor from "astro-compressor";
 // import partytown from "@astrojs/partytown";
@@ -18,23 +17,7 @@ export default defineConfig({
         windicss(),
         //critters(),
         vue(),
-        //partytown()
-        storyblok({
-            accessToken: "AM2jY0VgbjITiaic9Z5RTgtt",
-            components: {
-                hero: "components/Hero",
-                marketing1: "components/Marketing1",
-                landingPage: "storyblok/LandingPage",
-                blogPage: "storyblok/BlogPage",
-                blogPost: "components/subcomponents/BlogPost",
-                blogPostPreview: "components/BlogPostList",
-                advantageItem: "components/subcomponents/AdvantageItem",
-            },
-            apiOptions: {
-                region: "eu",
-            },
-            bridge: false,
-        }),
+        //partytown(),
         // purgecss(),
         compressor({
             gzip: false,
