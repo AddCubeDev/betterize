6d43e6a6740edca623d6d6b091abe956d1329539
ElDynamo <theDynamo2001@gmail.com>
2023-04-27 20:19:19 +0200

    wip

--- windi.config.ts
+++ windi.config.ts
@@ -7,8 +7,8 @@ export default defineConfig({
     darkMode: "class", // or 'media'
     shortcuts: {
         "header-1":
-            "text-4xl lg:text-6xl max-w-5xl font-bold text-gray-100 mx-auto uppercase",
-        "header-2": "text-lg lg:text-2xl text-gray-200 lg:px-32",
+            "text-4xl lg:text-6xl max-w-5xl font-bold text-gray-100 mx-auto uppercase text-center",
+        "header-2": "text-lg lg:text-2xl text-gray-200 text-left",
         "ordinary-text": "text-sm lg:text-md text-gray-300 max-w-2xl mx-auto",
         "button-cta": "px-16 py-4",
         "fancy-shadow":
