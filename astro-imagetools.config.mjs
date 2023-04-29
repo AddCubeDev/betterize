14a56c05a84da55fcafbf0c0f263e01c07e20f0c
pdodev <avatar4pl@gmail.com>
2023-04-29 08:43:57 +0200

    Add cache dir

--- astro-imagetools.config.mjs
+++ astro-imagetools.config.mjs
@@ -4,6 +4,7 @@ export default defineConfig({
     placeholder: "none", // jesli nie jest to ustawione, to na raz wyswietla sie zarowno placeholder jak i zwykly obrazek
     format: ["avif", "webp"],
     loading: "lazy",
+    cacheDir: "/.cache",
     // layout: "fill",
     includeSourceFormat: true,
     formatOptions: {
