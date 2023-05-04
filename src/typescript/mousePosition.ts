eb7574d1414ad8129253b01cedb1f132105d95c9
pdodev <avatar4pl@gmail.com>
2023-05-04 19:28:10 +0200

    Another improvement.

--- src/typescript/mousePosition.ts
+++ src/typescript/mousePosition.ts
@@ -7,34 +7,45 @@ export function traceMousePosition(args: TraceMousePositionArgs) {
     const element = document.getElementById(args.id);
 
     if (element) {
-        element.onmousemove = (e) => {
-            let cards = element.getElementsByClassName(args.class);
+        let cards = element.getElementsByClassName(args.class);
 
-            if (cards.length > 0) {
-                for (const card of cards) {
-                    if (card.matches(":hover")) {
-                        const rect = card.getBoundingClientRect(),
-                            x = e.clientX - rect.left,
-                            y = e.clientY - rect.top;
+        if (cards.length > 0) {
+            for (const card of cards) {
+                let card_with_style = card as HTMLDivElement;
 
-                        let card_with_style = card as HTMLDivElement;
+                var last_x = 0;
+                var last_y = 0;
+                const delta = 2;
+
+                card_with_style.onmousemove = (e) => {
+                    const rect = card_with_style.getBoundingClientRect();
+
+                    if (last_x == 0 || Math.abs(last_x - e.clientX) >= delta) {
+                        const x = e.clientX - rect.left;
+                        last_x = e.clientX;
 
                         card_with_style.style.setProperty(
                             "--mouse-x",
                             `${x}px`
                         );
+                    }
+
+                    if (last_y == 0 || Math.abs(last_y - e.clientY) >= delta) {
+                        const y = e.clientY - rect.top;
+                        last_y = e.clientY;
+
                         card_with_style.style.setProperty(
                             "--mouse-y",
                             `${y}px`
                         );
                     }
-                }
-            } else {
-                console.log(
-                    `traceMousePosition. No element found with class="${args.class}"!`
-                );
+                };
             }
-        };
+        } else {
+            console.log(
+                `traceMousePosition. No element found with class="${args.class}"!`
+            );
+        }
     } else {
         console.log(
             `traceMousePosition. Element with id="${args.id}" doesn't exist!`
