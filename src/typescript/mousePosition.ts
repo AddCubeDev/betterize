5c6f24993bbe2f18d02d3f44b0bcba6d1f77ce9e
pdodev <avatar4pl@gmail.com>
2023-05-04 20:12:10 +0200

    Dynamically register/unregister event listeners for 'mousemove' event for each 'card' element;

--- src/typescript/mousePosition.ts
+++ src/typescript/mousePosition.ts
@@ -3,43 +3,22 @@ interface TraceMousePositionArgs {
     class: string;
 }
 
-export function traceMousePosition(args: TraceMousePositionArgs) {
+export function createObserversForChildrenOfElementWithId(
+    args: TraceMousePositionArgs
+): IntersectionObserver[] {
     const element = document.getElementById(args.id);
+
+    var observers: IntersectionObserver[] = [];
 
     if (element) {
         let cards = element.getElementsByClassName(args.class);
 
         if (cards.length > 0) {
             for (const card of cards) {
-                let card_with_style = card as HTMLDivElement;
-
-                var last_x = 0;
-                var last_y = 0;
-                const delta = 2;
-
-                card_with_style.onmousemove = (e) => {
-                    const rect = card_with_style.getBoundingClientRect();
-
-                    if (last_x == 0 || Math.abs(last_x - e.clientX) >= delta) {
-                        const x = e.clientX - rect.left;
-                        last_x = e.clientX;
-
-                        card_with_style.style.setProperty(
-                            "--mouse-x",
-                            `${x}px`
-                        );
-                    }
-
-                    if (last_y == 0 || Math.abs(last_y - e.clientY) >= delta) {
-                        const y = e.clientY - rect.top;
-                        last_y = e.clientY;
-
-                        card_with_style.style.setProperty(
-                            "--mouse-y",
-                            `${y}px`
-                        );
-                    }
-                };
+                const observer = createObserverForMouseMove(
+                    card as HTMLDivElement
+                );
+                observers.push(observer);
             }
         } else {
             console.log(
@@ -51,4 +30,40 @@ export function traceMousePosition(args: TraceMousePositionArgs) {
             `traceMousePosition. Element with id="${args.id}" doesn't exist!`
         );
     }
+
+    return observers;
+}
+
+function createObserverForMouseMove(
+    card: HTMLDivElement
+): IntersectionObserver {
+    const observer = new IntersectionObserver(
+        (entries, _observer) => {
+            entries.forEach((entry) => {
+                if (entry.isIntersecting) {
+                    card.addEventListener("mousemove", handleMouseMove);
+                } else {
+                    card.removeEventListener("mousemove", handleMouseMove);
+                }
+            });
+        },
+        { threshold: 0 }
+    );
+
+    observer.observe(card);
+
+    return observer;
+}
+
+function handleMouseMove(event: MouseEvent) {
+    event.stopPropagation();
+
+    const card_with_style = event.target as HTMLDivElement;
+    const rect = card_with_style.getBoundingClientRect();
+
+    const x = event.clientX - rect.left;
+    const y = event.clientY - rect.top;
+
+    card_with_style.style.setProperty("--mouse-x", `${x}px`);
+    card_with_style.style.setProperty("--mouse-y", `${y}px`);
 }
