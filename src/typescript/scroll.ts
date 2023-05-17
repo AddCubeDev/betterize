b6d1adf84f1905c331c72106d0c416b4b0b78146
pdodev <avatar4pl@gmail.com>
2023-05-17 06:12:55 +0200

    Add parameter to showWhenInView

--- src/typescript/scroll.ts
+++ src/typescript/scroll.ts
@@ -1,15 +1,61 @@
-export { showWhenInView };
+export { showWhenInView, CSS_CLASS_SHOW };
 
-const observer = new IntersectionObserver((entries, observer) => {
-    entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-            entry.target.classList.add("pokaz");
-            observer.unobserve(entry.target); // przestajemy obserwować element, gdy został już pokazany
-        }
-    });
-});
+// ----------------------------------------------------------------------------
+// To 'register' new CSS class:
+// 1. Add new CSS_CLASS_XXX definition
+// 2. Add this definition to the knownCssClasses list
+// E.g.:
+// const CSS_CLASS_SHOW2 = "pokaz2";
+// ...
+// const knownCssClasses = [CSS_CLASS_SHOW, CSS_CLASS_SHOW2];
+//
+const CSS_CLASS_SHOW = "pokaz";
+const knownCssClasses = [CSS_CLASS_SHOW];
 
-function showWhenInView(css_class: string): void {
-    const notYetShownElements = document.querySelectorAll(css_class);
+// ----------------------------------------------------------------------------
+type CssClassName = string;
+type Observers = Map<CssClassName, IntersectionObserver>;
+
+const observers = buildObservers(knownCssClasses);
+
+// ----------------------------------------------------------------------------
+
+function showWhenInView(
+    queriedClass: CssClassName,
+    showClass: CssClassName
+): void {
+    const observer = observers.get(showClass);
+
+    if (observer == undefined) {
+        console.error(
+            `Developer errror. Unknown CSS class "${showClass}". Known classes in scroll.ts: ${knownCssClasses}`
+        );
+        return;
+    }
+
+    const notYetShownElements = document.querySelectorAll(queriedClass);
     notYetShownElements.forEach((el) => observer.observe(el));
+}
+
+// ----------------------------------------------------------------------------
+
+function buildObservers(classNames: CssClassName[]): Observers {
+    const observers: Observers = new Map<CssClassName, IntersectionObserver>();
+
+    classNames.forEach((className) =>
+        observers.set(className, buildObserver(className))
+    );
+
+    return observers;
+}
+
+function buildObserver(className: CssClassName): IntersectionObserver {
+    return new IntersectionObserver((entries, observer) => {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add(className);
+                observer.unobserve(entry.target); // przestajemy obserwować element, gdy został już pokazany
+            }
+        });
+    });
 }
